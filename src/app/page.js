8a49e5fc8fc736ba4e34968ff736b87b079f0527
sujal mallick <mallicksujal626@gmail.com>
2026-02-19 11:12:06 +0530

    fix: update redirect URL for Google authentication to use dynamic origin

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "http://localhost:3000/dashboard",
+        redirectTo: `${location.origin}/dashboard`,
       },
     });
   };
